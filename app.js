ae8906feee2c3606ba4a88a1bb76e3e0f4b524cc
Rafael Pacas @rapala61 <rapala61@gmail.com>
2016-10-18 22:43:29 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const indexRouter = require('./routes/index.js');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(port);
