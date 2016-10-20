ec01b5188caf7575477c8233fcf383ff5b179861
Rafael Pacas @rapala61 <rapala61@gmail.com>
2016-10-19 20:49:56 -0400

    refactored the producer and implemented the post route

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const path = require('path');
@@ -10,7 +12,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// set engine to output ejs
 app.set('view engine', 'ejs');
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
+const Producer = require('../lib/producer.js');
+
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.post('/', (req, res) => {
+  const payload = {
+    name: req.body.name,
+    dob: req.body.dob,
+    email: req.body.email,
+    cell: req.body.cell,
+    password: req.body.password,
+    password_confirm: req.body.password_confirm
+  };
+
+  const publisher = new Producer();
+  const exchange = 'form_submission';
+  const key = 'sign_up';
+  const messageObject = {
+    exchange,
+    routingKey: key,
+    payload: JSON.stringify(payload),
+    options: {
+      contentType: 'aplication/json',
+      timestamp: Date.now()
+    }
+  };
+
+  publisher.sendMessage(messageObject, (err, ok) => {
+    if (err !== null) {
+      res.status(500);
+    } else {
+      res.json({ status: 200 });
+    }
+  });
 });
 
 module.exports = router;
